cd2f1e0e05f07a3fafef7c1d19d42f95d55aa611
HARSH17m <harshmistry137@gmail.com>
2025-06-17 12:19:12 +0530

    UPDATED BASE.html

--- apps/expense/views.py
+++ apps/expense/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render,redirect
 # Create your views here.
 def index(request):
     return render(request,'expense/index.html')
+def analytics(request):
+    return render(request,'expense/analytics.html')
