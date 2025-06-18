5d72758a86245d7f178935958d7a135c639fe9da
HARSH17m <harshmistry137@gmail.com>
2025-06-18 21:52:38 +0530

    Changes added

--- apps/expense/views.py
+++ apps/expense/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.hashers import make_password, check_password
 from django.core.mail import send_mail
 from django.conf import settings
 
+from .models import Users
+
 from .helpers import *
 
 import random
@@ -13,7 +15,24 @@ def login(request):#VIVEK
         email_=request.POST['email']
         password_=request.POST['password']
         #
+        if not Users.objects.filter(email=email_).exists():
+            print("Email does't exist")
+            return redirect('login')
         
+        get_user = Users.objects.get(email=email_)
+
+        if not get_user.is_active:
+            print("Your account is deactive please contact to customer care")
+            return redirect('login')
+        
+        is_password_verify = check_password(password_, get_user.password)
+        if not is_password_verify:
+            print("Email or password not match")
+            return redirect('login') 
+        
+        request.session['user_id'] = str(get_user.UID)
+        return redirect('index')
+        #
     return render(request,'expense/login.html')
 
 def signup(request):#ADAM
@@ -22,7 +41,41 @@ def signup(request):#ADAM
         password_=request.POST['password']
         confirm_password_=request.POST['confirm_password']
         #
+        if not is_email_verified:
+            print("Invalid email ")
+            return redirect('signup')
 
+        if Users.objects.filter(email=email_).exists():
+            print("Email already exist")
+            return redirect('signup')
+
+        if password_ != confirm_password_:
+            print("Password does not match")
+            return redirect('signup')
+
+        if not is_valid_password(password_)[0]:
+            print(is_valid_password(password_)[1])
+            return redirect('signup')
+        
+        otp_ = random.randint(111111,999999)
+
+        user = Users(
+            email=email_,
+            password=make_password(password_),
+            otp=otp_
+        )
+        subject = "Email Confirmation mail | Expenseo"
+        message = f"OTP | {otp_}"
+        from_email = settings.EMAIL_HOST_USER
+        recipient_list = [f"{email_}"]
+        if send_mail(subject,message,from_email,recipient_list):
+            print("Email Sent")
+            user.save()
+            context={
+                'email':email_
+            }
+            return render(request,'expense/email_verify.html',context)
+        #        
     return render(request,'expense/signup.html')
 
 def email_verify(request):#VARUN
@@ -30,7 +83,21 @@ def email_verify(request):#VARUN
         email_=request.POST['email']
         otp_=request.POST['otp']
         #
+        user = Users.objects.get(email=email_)
 
+        if int(otp_) != int(user.otp):
+            print(user.otp)
+            print(otp_)
+            print("Invalid OTP")
+            context = {
+            'email': email_
+            }
+            return render(request,'expense/email_verify.html', context)
+
+        user.is_active = True
+        user.save()
+        return redirect('login')
+        #
     return render(request,'expense/email_verify.html')
 
 def forgot_password(request):#HARSH
