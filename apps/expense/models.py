a3be99422182a5b547202eab4551e160decffd1d
HARSH17m <harshmistry137@gmail.com>
2025-06-18 12:28:22 +0530

    Updates

--- apps/expense/models.py
+++ apps/expense/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import uuid
 # Create your models here.
 class BaseClass(models.Model):
-    UUID=models.UUIDField(default=uuid.uuid4,primary_key=True,null=False,blank=False)
+    UID=models.UUIDField(default=uuid.uuid4,primary_key=True,null=False,blank=False)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
     class Meta:
@@ -11,3 +11,7 @@ class Users(BaseClass):
     email=models.EmailField(max_length=255,null=False,blank=False)
     password=models.CharField(max_length=255,null=False,blank=False)
     is_active=models.BooleanField(default=False)
+
+# class UserDetails(Users):
+#     Uid=models.ForeignKey(Users,on_delete=models.CASCADE)
+
